76411b34dd8a58122acf86c513f2c56fde5292c6
SreeHari <sreehari400.sh@gmail.com>
2021-10-19 23:55:07 +0530

    Static files and theme install

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,8 +1,19 @@
 from django.db.models import fields
-from django.forms import ModelForm
+from django.db.models.base import Model
+from django import forms
+from django.forms import ModelForm, widgets
 from .models import *
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title','description', 'demo_link', 'source_link' ,'tags']
+        fields = ['title', 'featured_image','description', 'demo_link', 'source_link' ,'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+        
+        def __init__(self, *args, **kwargs):
+            super(ProjectForm, self).__init__(*args, **kwargs)
+            
+            for name, field in self.fields.items():
+                field.widget.attrs.update({'class':'input'})
